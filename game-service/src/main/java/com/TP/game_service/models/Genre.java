d1eea030ddde1e602ae163f0fd6933731827e662
MaluAbrami <marialuizaabrami@gmail.com>
2025-05-08 15:27:44 -0300

    add class game, genre, plataform

---
+++ game-service/src/main/java/com/TP/game_service/models/Genre.java
@@ -0,0 +1,16 @@
+package com.TP.game_service.models;
+
+import java.util.UUID;
+
+public class Genre {
+
+    public Genre(int id, String name, String image_background) {
+        this.id = id;
+        this.name = name;
+        this.image_background = image_background;
+    }
+
+    private int id;
+    private String name;
+    private String image_background;
+}
---
+++ game-service/src/main/java/com/TP/game_service/models/Plataform.java
@@ -0,0 +1,14 @@
+package com.TP.game_service.models;
+
+public class Plataform {
+
+    public Plataform(int id, String name, String image_background) {
+        this.id = id;
+        this.name = name;
+        this.image_background = image_background;
+    }
+
+    private int id;
+    private String name;
+    private String image_background;
+}
