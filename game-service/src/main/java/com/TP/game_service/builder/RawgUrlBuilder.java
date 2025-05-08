03ed36f622fc7f04af0d9b47c5e07dd64f7d37ce
MaluAbrami <marialuizaabrami@gmail.com>
2025-05-08 15:47:27 -0300

    add Builder

---
+++ game-service/src/main/java/com/TP/game_service/builder/RawgUrlBuilder.java
@@ -0,0 +1,32 @@
+package com.TP.game_service.builder;
+
+import java.util.Properties;
+
+public class RawgUrlBuilder {
+    private final StringBuilder url;
+    private Properties props = new Properties();
+
+    public RawgUrlBuilder() {
+        url = new StringBuilder("https://api.rawg.io/api/");
+    }
+
+    public RawgUrlBuilder page(int page) {
+        url.append("games?page=").append(page);
+        return this;
+    }
+
+    public RawgUrlBuilder genre(String genre) {
+        url.append("games?genre=").append(genre);
+        return this;
+    }
+
+    public RawgUrlBuilder search(String query) {
+        url.append("games?search=").append(query);
+        return this;
+    }
+
+    public String build() {
+        url.append(props.getProperty("&" + "rawg.api.key"));
+        return url.toString();
+    }
+}
