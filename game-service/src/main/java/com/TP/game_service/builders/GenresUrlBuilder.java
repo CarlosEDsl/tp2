5f4692bc5d4fd6cc523224706c0151b89546da9e
Malu Abrami <marialuizaabrami@gmail.com>
2025-06-20 09:35:00 -0300

    Criado novos builders de url para platform e genre

---
+++ game-service/src/main/java/com/TP/game_service/builders/GenresUrlBuilder.java
@@ -0,0 +1,42 @@
+package com.TP.game_service.builders;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GenresUrlBuilder {
+    private final StringBuilder url;
+    private String rawgApiKey;
+
+    public GenresUrlBuilder(String apiKey) {
+        url = new StringBuilder("https://api.rawg.io/api/genres?");
+        this.rawgApiKey = apiKey;
+    }
+
+    public GenresUrlBuilder getById(Long id) {
+        url.setLength(0);
+        url.append("https://api.rawg.io/api/genres/")
+                .append(id)
+                .append("?");
+        return this;
+    }
+
+    public GenresUrlBuilder page(int page) {
+        url.append("&page=").append(page);
+        return this;
+    }
+
+    public GenresUrlBuilder pageSize(int pageSize) {
+        url.append("&page_size=").append(pageSize).append("&");
+        return this;
+    }
+
+    public GenresUrlBuilder ordering(String order) {
+        url.append("&ordering=-").append(order).append("&");
+        return this;
+    }
+
+    public String build() {
+        url.append("&key=").append(rawgApiKey);
+        return url.toString();
+    }
+}
---
+++ game-service/src/main/java/com/TP/game_service/builders/PlatformsUrlBuilder.java
@@ -0,0 +1,42 @@
+package com.TP.game_service.builders;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlatformsUrlBuilder {
+    private final StringBuilder url;
+    private String rawgApiKey;
+
+    public PlatformsUrlBuilder(String apiKey) {
+        url = new StringBuilder("https://api.rawg.io/api/platforms?");
+        this.rawgApiKey = apiKey;
+    }
+
+    public PlatformsUrlBuilder getById(Long id) {
+        url.setLength(0);
+        url.append("https://api.rawg.io/api/platforms/")
+                .append(id)
+                .append("?");
+        return this;
+    }
+
+    public PlatformsUrlBuilder page(int page) {
+        url.append("&page=").append(page);
+        return this;
+    }
+
+    public PlatformsUrlBuilder pageSize(int pageSize) {
+        url.append("&page_size=").append(pageSize).append("&");
+        return this;
+    }
+
+    public PlatformsUrlBuilder ordering(String order) {
+        url.append("&ordering=-").append(order).append("&");
+        return this;
+    }
+
+    public String build() {
+        url.append("&key=").append(rawgApiKey);
+        return url.toString();
+    }
+}
